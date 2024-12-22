fa3eef7053fc9391be686f0b2278fbda9369f733
WOZBORN <wolker2015@gmail.com>
2024-12-22 15:04:22 +0700

    done homework

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import operator
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -27,13 +28,46 @@ async def help_command(client: Client, message: Message):
     await message.reply(
         "Я умею:\n"
         "/help - показать команды\n"
-        "/date - напоминать дату"
+        "/date - напоминать дату\n"
+        "/calc - калькулятор"
     )
 
 
 @app.on_message(filters.command("date"))
 async def date_command(client: Client, message: Message):
     await message.reply(f"Сегодня {datetime.now().strftime('%d.%m.%Y')}")
+
+
+@app.on_message(filters.command("calc"))
+async def calc_command(client: Client, message: Message):
+    content = message.text.split()
+
+    operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv
+    }
+
+    if len(content) != 4 or not content[1].isdigit() or not content[3].isdigit() or content[2] not in operators:
+        await message.reply(
+            "Неправильное написал пример. Попробуй такие:\n"
+            "/calc 2 + 2\n"
+            "/calc 2 * 2\n"
+            "/calc 2 - 2\n"
+            "/calc 2 / 2"
+        )
+
+    num1 = int(content[1])
+    num2 = int(content[3])
+    op = content[2]
+    try:
+        result = operators[op](num1, num2)
+    except ZeroDivisionError:
+        await message.reply("На ноль делить нельзя!")
+        return
+
+    await message.reply(f"{num1} {op} {num2} = {result}")
 
 
 @app.on_message(filters.text)
